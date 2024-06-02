9d09bef57a8f8cbd29131f44cc0376c17e3e1c9b
arpit1107 <arpit1107programming@gmail.com>
2024-06-02 17:23:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = 3000 | process.env.PORT;
+
+app.get('/',(req,res)=>{
+  res.send("hello world");
+})
+
+app.listen(PORT,(error)=>{
+  if(!error){
+    console.log(`server is runing at ${PORT}`);
+  }else{
+    console.log("Error: server is not started!!!");
+  }
+})
