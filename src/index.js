092845d17011fab5e306ce51cab12bd11a0177cd
arpit1107 <arpit1107programming@gmail.com>
2024-06-07 21:37:13 +0530

    Server code is added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import dotenv from "dotenv";
 import connect_db from "./db/index.js";
+// import { app } from "./index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: './env'
 })
 
+const PORT = process.env.PORT || 8000;
 
-connect_db();
+connect_db()
+.then(() => {
+  app.listen(PORT,(error) => {
+    if(!error){
+      console.log(`Server is running on ${PORT}`);
+    }else{
+      console.log("Server Error",error);
+    }
+  })
+})
+.catch((error) => {
+  console.log("Error is DB Connection",error);
+})
+
 
 
 
